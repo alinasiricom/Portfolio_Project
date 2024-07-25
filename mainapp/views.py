7cbc5fc67f93922d49572be88ca30acefb3ec3d2
alinasiricom <nasiri.ali.r@gmail.com>
2024-07-25 12:56:26 +0330

    change var name
    change var name content to context

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def index_view(request):
-    content = {
+    context = {
         'full_name':'Ali Nasiri', 
         'job_title':'Back-end Developer',
         'email':'nasiri.ali.r@gmail.com',
         'Location':'Tehran',
         }
-    return render(request, 'mainapp/index.html', content)
+    return render(request, 'mainapp/index.html', context)
